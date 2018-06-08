5a493243856a68b39e848521209e398c678243b5
SirNightmare <38385282+SirNightmare@users.noreply.github.com>
2018-06-08 20:26:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
   }
+       
+         let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+  if(!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+      prefixes: botconfig.prefix
+    };
+  }
 
   if(!coins[message.author.id]){
     coins[message.author.id] = {
